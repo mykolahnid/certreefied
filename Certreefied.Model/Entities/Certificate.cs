14f439f0a4bd702879b018b6a327ad8c8e971cea
mykolahnid <g.mykola@gmail.com>
2017-04-30 23:45:41 +0300

    solution and projects

---
+++ Certreefied.Model/Entities/Certificate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Certreefied.Model.Entities
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
